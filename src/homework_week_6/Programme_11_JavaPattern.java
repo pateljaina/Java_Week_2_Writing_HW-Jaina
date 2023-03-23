2cf6b133e9efae41ba7ac38c4855988163066f6c
pateljaina <pateljaina@hotmail.co.uk>
2023-03-23 20:44:53 +0000

    message

---
+++ src/homework_week_6/Programme_11_JavaPattern.java
@@ -0,0 +1,17 @@
+package homework_week_6;
+
+public class Programme_11_JavaPattern {
+    public static void main(String[] args) {
+        Programme_11_JavaPattern t = new Programme_11_JavaPattern();
+        System.out.println("---------------------------------------------");
+        t.javaPattern();
+    }
+
+    // Printing the Java Pattern
+    public void javaPattern() {
+        System.out.println("J  a  v   v a");
+        System.out.println("J a a  v v a a");
+        System.out.println("J J aaaaa  V V aaaaa");
+        System.out.println("J J a   a  V a  a");
+    }
+}
